a3b1fcb306b24d150fd0b95c092375d36eb4208b
Dan Bolser <dan.bolser@gmail.com>
2011-04-27 01:03:41 +0100

    attempt to simplify code
    I'll need to do this properly when I'm actually running the code on a test wiki
    
    I also need to look at security issues

--- gbrowseimage.body.php
+++ gbrowseimage.body.php
@@ -17,57 +17,8 @@ class gbrowseImage {
   // wikitext to use as a caption (probably a transclusion)
   var $caption;
   
-  // will hold all the options for gbrowse_img, assoc. array
-  var $query = array();
-  
-  
   // gbrowse_img arguments
-  
-  // the source of the data, never null
-  var $source;
-  
-  // genomic landmark or range
-  var $name;
-  
-  // an array, order dependent, possibly null
-  var $coordinates;
-  
-  // tracks to include in image
-  var $type;
-  
-  // desired width of image
-  var $width;
-  
-  // list of track options (compact, labeled, etc)
-  var $options;
-  
-  // display position in absolute coordinates
-  var $abs;
-  
-  // added feature(s) to superimpose on the image
-  var $add;
-  
-  // stylesheet for additional features
-  var $style;
-
-  // where to place the image key
-  var $keystyle;
-  
-  // force an overview-style display
-  var $overview;
-  
-  // bool, whether to reverse the image or not
-  var $flip;
-  
-  // turn grid on (1) or off (0)
-  var $grid;
-  
-  // generate full HTML for image and imagemap for use in an embedded
-  // frame
-  var $embed;
-  
-  // format for the image (use "SVG" for scaleable vector graphics)
-  var $format;
+  var $gbArgs = array();
   
   
   
@@ -93,61 +44,7 @@ class gbrowseImage {
 	$option = trim($m[1]);
 	$value  = trim($m[2]);
 	
-	switch ( $option ) {
-	case "source":
-	  $this->source = $value;
-	  break;
-	case "name":
-	  $this->name = $value;
-	  break;
-	case "width":
-	  // get the integer value of whatever came in
-	  $this->width = intval( $value );
-	  break;
-	case "type":
-	  $this->type = $value;
-	  break;
-	case 'options':
-	  $this->options = $value;
-	  break;
-	case 'abs':
-	  $this->abs = $value;
-	  break;
-	case 'add':
-	  $this->add = $value;
-	  break;
-	case 'style':
-	  $this->style = $value;
-	  break;
-	case 'keystyle':
-	  $this->keystyle = $value;
-	  break;
-	case 'overview':
-	  $this->overview = $value;
-	  break;
-	case 'grid':
-	  $this->grid = $value;
-	  break;
-	case 'format':
-	  $this->format = $value;
-	  break;
-	case "embed":
-	  $this->embed = $value;
-	  break;
-	case "flip":
-	  // if there is anything that evaluates to true, use it
-	  if ( $value ) {
-	    $this->flip = true;
-	  }
-	  break;
-	case "preset":
-	  $this->preset = $value;
-	  break;
-	case 'caption':
-	  // could be problematic because of multiple lines....oh well.
-	  $this->caption = $value;
-	  break;
-	}
+	$this->$gbArgs[$option] = $value;
       }
     }
   }	// done parsing parameters
@@ -163,57 +60,27 @@ class gbrowseImage {
     $this->parseInput( $this->input );
     
     // check for required parameters
-    if ( !isSet($this->source) ) {
+    if ( !isSet($this->gbArgs['source']) ) {
       trigger_error( 'no source set, cannot continue', E_USER_WARNING );
       return $this->makeErrorString( 'No \'source\' parameter set. Cannot make image.' );
     }
-    if ( !isSet($this->name) ) {
+    
+    if ( !isSet($this->gbArgs['name']) ) {
       trigger_error( 'no name set, cannot continue', E_USER_WARNING );
       return $this->makeErrorString( 'No \'name\' parameter set. Cannot make image.' );
     }
     
-    // set up the basic/default options for gbrowse_img into an array
-    $this->query['name'] = $this->name;
-    $this->query['width'] = ( isSet($this->width) && $this->width )
-      ? $this->width
+    $this->gbArgs['width'] = ( isSet($this->gbArgs['width']) && $this->gbArgs['width'] )
+      ? $this->gbArgs['width']
       : $wgGbrowseImageSize;
     
     // Setting a default type is ... tricky... Would be nice to query
     // the script for types if no user-type is supplied
-    $this->query['type'] = ( isSet($this->type) && $this->type )
-      ? $this->type
+    $this->gbArgs['type'] = ( isSet($this->gbArgs['type']) && $this->gbArgs['type'] )
+      ? $this->gbArgs['type']
       : "Genes+Genes:region+ncRNA+ncRNA:region";
     
-    if ( isSet($this->options) ) {
-      $this->query['options'] = $this->options;
-    }
-    if ( isSet($this->abs) ) {
-      $this->query['abs'] = $this->abs;
-    }
-    if ( isSet($this->add) ) {
-      $this->query['add'] = $this->add;
-    }
-    if ( isSet($this->style) ) {
-      $this->query['style'] = $this->style;
-    }
-    if ( isSet($this->keystyle) ) {
-      $this->query['keystyle'] = $this->keystyle;
-    }
-    if ( isSet($this->flip) ) {
-      $this->query['flip'] = $this->flip;
-    }
-    if ( isSet($this->grid) ) {
-      $this->query['grid'] = $this->grid;
-    }
-    if ( isSet($this->embed) ) {
-      $this->query['embed'] = $this->embed;
-    }
-    if ( isSet($this->format) ) {
-      $this->query['format'] = $this->format;
-    }
-    
-    
-    // make the HTML
+     // make the HTML
     $html = '<a href="' . $this->makeGbrowseURL() . '" target="_blank">
 		            <img src="' . $this->makeGbrowseImgURL() . '" alt="' . $this->makeGbrowseURL() . '" />
 		        </a>';
@@ -221,8 +88,8 @@ class gbrowseImage {
     // for debugging
     #$html .= '<br />' . htmlentities($this->makeGbrowseImgURL());
     
-    $html .= ( isSet($this->caption) && $this->caption )
-      ? "\n" . $this->caption
+    $html .= ( isSet($this->gbArgs['caption']) && $this->gbArgs['caption'] )
+      ? "\n" . $this->gbArgs['caption']
       : "";
     
     wfProfileOut( __METHOD__ );
@@ -241,7 +108,7 @@ class gbrowseImage {
   // take care of the rest...
   // 
   protected function formatTypeParameter() {
-    $tracks = explode( '+', $this->query['type'] );
+    $tracks = explode( '+', $this->gbArgs['type'] );
     $string = "";
     for ( $i=0, $c=count($tracks); $i<$c; $i++ ) {
       if ( $i != 0 ) {
@@ -249,32 +116,20 @@ class gbrowseImage {
       }
       $string .= $tracks[$i];
     }
-    $this->query['type'] = $string;
-    
+    $this->gbArgs['type'] = $string;
+
   }
   
-  
-  
-  // use like this: list($chromosome, $coordA, $coordB) =
-  // $this->parseLandmark( $landmark );
-  protected function parseLandmark( $name ) {
-    if ( preg_match( '/(.*):(\d+)\.\.(\d+)/', $name, $m ) ) {
-      return array_splice( $m, 1, 3 ); // return $m[1..3]
-    }
-    else {
-      return false;
-    }
-  }
   
   protected function makeGbrowseImgURL() {
-    $base = $wgGbrowseImageScriptURL . '/' . $this->source;
+    $base = $wgGbrowseImageScriptURL . '/' . $this->gbArgs['source'];
     $this->formatTypeParameter();
-    $url = $base . '/?' . http_build_query( $this->query );
+    $url = $base . '/?' . http_build_query( $this->gbArgs );
     return urldecode($url);
   }
   
   protected function makeGbrowseURL() {
-    $base = $wgGbrowseImageGbrowseURL . '/' . $this->source;
+    $base = $wgGbrowseImageGbrowseURL . '/' . $this->gbArgs['source'];
     $url = $base . '/?name=' . $this->name;
     return urldecode($url);
   }
